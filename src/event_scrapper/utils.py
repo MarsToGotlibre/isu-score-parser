f41d9bc11581dd44de0989978928fea0b12086a6
MarsToGotlibre <231478721+MarsToGotlibre@users.noreply.github.com>
2026-01-31 00:07:21 +0100

    [Feat] - Found and add timezone

--- src/event_scrapper/utils.py
+++ src/event_scrapper/utils.py
@@ -1,6 +1,7 @@
 from datetime import datetime as datetime
 import pandas as pd
 import requests
+import re
 
 def return_iso_date(str_date):
     date=""
@@ -13,6 +14,21 @@ def return_iso_date(str_date):
             date=str_date.strip()
                 #logging.warning(f""Couldn't put date into iso format, falling back to extracted date.")
     return date
+
+def found_timezone_date(soup,dic):
+    date = re.compile(r"(\d{2}\.\d{2}\.\d{4}) - (\d{2}\.\d{2}\.\d{4})")
+    for td in soup.find_all("td"):
+        d= date.match(td.text.strip())
+        if d :
+            assert len(d.groups())==2
+            dic["start_date"] = return_iso_date(d.groups()[0])
+            dic["end_date"] = return_iso_date(d.groups()[1])
+            continue
+        if "Local Time" in td.text:
+            timezone=td.text.strip("()").split(",")
+            if len(timezone)>1:
+                dic["timezone"] = timezone[1].strip()
+            return
 
 def empty_cell_to_nan(x):
     if not isinstance(x,tuple):
