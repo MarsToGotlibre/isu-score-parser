80fffd600812b9250390a6824aaa429c590339b1
MarsToGotlibre <231478721+MarsToGotlibre@users.noreply.github.com>
2026-01-31 17:03:31 +0100

    [Feat] - event scrapper json name support

--- src/utils/filename.py
+++ src/utils/filename.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import date
 @dataclass
 class FilenameGenerator:
     
@@ -8,12 +9,14 @@ class FilenameGenerator:
     segment:str | None =None
     compet:str | None=None
     div:str | None=None
-    date:str | None = None
+    year:str | None = None
     cat:str |None =None
+    place:str | None = None
+    begindate:date | None =  None
 
     @property
     def pdf_results_directory(self):
-        parts = [ self.compet, self.date,self.segment,self.cat, self.div, self.discipline]
+        parts = [ self.compet, self.year,self.segment,self.cat, self.div, self.discipline]
         directory="_".join(p for p in parts if p) if any(parts) else ""
         return directory
     
@@ -21,8 +24,12 @@ class FilenameGenerator:
     def json_filename(self):
         
         
-        parts = [self.cat, self.div, self.discipline, self.compet, self.date]
+        parts = [self.cat, self.div, self.discipline, self.compet, self.year]
         optional = "_" + "_".join(p for p in parts if p) if any(parts) else ""
         
         info=f"{self.name}_{self.country}_{self.segment}"
         return f"{info}{optional}.json"
+    
+    @property
+    def event_json(self):
+        return f"{self.begindate.isoformat()}_{"-".join(self.compet.lower().split()).strip()}"
