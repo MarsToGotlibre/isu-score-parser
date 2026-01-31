8f136d8aad061b7ff9fb4f995cc3fa23f8714174
MarsToGotlibre <231478721+MarsToGotlibre@users.noreply.github.com>
2026-01-31 19:26:38 +0100

    [Refactor] - suppresed domain, specific function, changed type date output

--- src/event_scrapper/utils.py
+++ src/event_scrapper/utils.py
@@ -9,29 +9,15 @@ logger=logging.getLogger(__name__)
 def return_iso_date(str_date):
     date=""
     try:
-        date=datetime.strptime(str_date.strip(),"%d.%m.%Y").date().isoformat()
+        date=datetime.strptime(str_date.strip(),"%d.%m.%Y").date()
     except ValueError:
         try:
-            date=datetime.strptime(str_date.strip(),"%d/%m/%Y").date().isoformat()
+            date=datetime.strptime(str_date.strip(),"%d/%m/%Y").date()
         except ValueError:
             date=str_date.strip()
             logger.warning(f"Couldn't put date into iso format, falling back to raw extracted date.")
     return date
 
-def found_timezone_date(soup,dic):
-    date = re.compile(r"(\d{1,2}\.\d{1,2}\.\d{4}) - (\d{1,2}\.\d{1,2}\.\d{4})")
-    for td in soup.find_all("td"):
-        d= date.match(" ".join(td.text.split()).strip())
-        if d :
-            assert len(d.groups())==2
-            dic["start_date"] = return_iso_date(d.groups()[0])
-            dic["end_date"] = return_iso_date(d.groups()[1])
-            continue
-        if "Local Time" in td.text:
-            timezone=td.text.strip("()").split(",")
-            if len(timezone)>1:
-                dic["timezone"] = timezone[1].strip()
-            return
 
 def empty_cell_to_nan(x):
     if not isinstance(x,tuple):
